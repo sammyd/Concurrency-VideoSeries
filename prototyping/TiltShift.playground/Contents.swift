2956adbc190a075ef3e61c886555da52f8fbf9c1
Sam Davies <sam@razeware.com>
2015-11-17 17:30:09 +0700

    Demonstrating multiple blocks in NSBlockOperation

--- prototyping/TiltShift.playground/Contents.swift
+++ prototyping/TiltShift.playground/Contents.swift
@@ -5,11 +5,6 @@ import XCPlayground
 
 let inputImage = UIImage(named: "dark_road_small.jpg")!
 
-let mask = topAndBottomGradient(inputImage.size, clearLocations: [0.35, 0.65], innerIntensity: 0.8)
-
-
-let blurred = inputImage.applyBlurWithRadius(3, maskImage: mask)
-
 var result: Int?
 
 let calculateSum = NSBlockOperation(block: {
@@ -19,10 +14,40 @@ let calculateSum = NSBlockOperation(block: {
 
 result
 
-let startTime = NSDate()
+var startTime = NSDate()
 calculateSum.start()
 NSDate().timeIntervalSinceDate(startTime)
 
 result
 
+let multiPrinter = NSBlockOperation()
+multiPrinter.addExecutionBlock { print("Hello"); sleep(5) }
+multiPrinter.addExecutionBlock { print("my"); sleep(5) }
+multiPrinter.addExecutionBlock { print("name"); sleep(5) }
+multiPrinter.addExecutionBlock { print("is"); sleep(5) }
+multiPrinter.addExecutionBlock { print("sam"); sleep(5) }
 
+startTime = NSDate()
+multiPrinter.start()
+NSDate().timeIntervalSinceDate(startTime)
+
+
+class BlurOperation: NSOperation {
+  var inputImage: UIImage?
+  var outputImage: UIImage?
+  
+  override func main() {
+    guard let inputImage = inputImage else { return }
+    
+    let mask = topAndBottomGradient(inputImage.size, clearLocations: [0.35, 0.65], innerIntensity: 0.8)
+    outputImage = inputImage.applyBlurWithRadius(3, maskImage: mask)
+  }
+}
+
+let blur = BlurOperation()
+blur.inputImage = inputImage
+blur.outputImage
+startTime = NSDate()
+blur.start()
+NSDate().timeIntervalSinceDate(startTime)
+blur.outputImage
