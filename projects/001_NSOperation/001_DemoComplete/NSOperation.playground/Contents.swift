88c7178298440e79707fb5a046a5238aad951e15
Sam Davies <sam@razeware.com>
2015-11-17 18:21:28 +0700

    Completed 001_Demo - NSOperation

--- projects/001_NSOperation/001_DemoComplete/NSOperation.playground/Contents.swift
+++ projects/001_NSOperation/001_DemoComplete/NSOperation.playground/Contents.swift
@@ -7,17 +7,30 @@ import UIKit
 var result: Int?
 
 //: Creating an NSBlockOperation to add two numbers
+let summationOperation = NSBlockOperation(block: {
+  result = 2 + 3
+  sleep(5)
+})
 
+startClock()
+summationOperation.start()
+stopClock()
 
-
-
-
+result
 
 //: NSBlockOperations can have multiple blocks, that run concurrently
 
 let multiPrinter = NSBlockOperation()
 
+multiPrinter.addExecutionBlock({ print("Hello"); sleep(5) })
+multiPrinter.addExecutionBlock({ print("my"); sleep(5) })
+multiPrinter.addExecutionBlock({ print("name"); sleep(5) })
+multiPrinter.addExecutionBlock({ print("is"); sleep(5) })
+multiPrinter.addExecutionBlock({ print("Sam"); sleep(5) })
 
+startClock()
+multiPrinter.start()
+stopClock()
 
 
 //: ## Subclassing NSOperation
@@ -26,6 +39,25 @@ let inputImage = UIImage(named: "dark_road_small.jpg")
 
 //: Creating an operation to add tilt-shift blur to an image
 
+class TiltShiftOperation : NSOperation {
+  var inputImage: UIImage?
+  var outputImage: UIImage?
+  
+  override func main() {
+    guard let inputImage = inputImage else { return }
+    let mask = topAndBottomGradient(inputImage.size)
+    outputImage = inputImage.applyBlurWithRadius(4, maskImage: mask)
+  }
+}
+
+let tsOp = TiltShiftOperation()
+tsOp.inputImage = inputImage
+
+startClock()
+tsOp.start()
+stopClock()
+
+tsOp.outputImage
 
 
 
