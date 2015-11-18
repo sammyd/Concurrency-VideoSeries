8f6532eb80c4a401287f871abe89e9e4872e633a
Sam Davies <sam@razeware.com>
2015-11-18 17:58:50 +0700

    Replaces the jpgs with compressed files

--- prototyping/Compression.playground/Contents.swift
+++ prototyping/Compression.playground/Contents.swift
@@ -2,14 +2,17 @@ import Foundation
 import XCPlayground
 import Compressor
 
-let outputURL = XCPlaygroundSharedDataDirectoryURL.URLByAppendingPathComponent("dark_road_small.compressed")
+
+let filename = "sample_09"
+
+let outputURL = XCPlaygroundSharedDataDirectoryURL.URLByAppendingPathComponent("\(filename).compressed")
 
 //let inputImage = NSImage(named: "dark_road_small.jpg")!
-let imagePath = NSBundle.mainBundle().pathForResource("dark_road_small", ofType: "jpg")
+//let imagePath = NSBundle.mainBundle().pathForResource("dark_road_small", ofType: "jpg")
 
-let imageData = NSData(contentsOfFile: NSBundle.mainBundle().pathForResource("dark_road_small", ofType: "jpg")!)
+let imageData = NSData(contentsOfFile: NSBundle.mainBundle().pathForResource(filename, ofType: "jpg")!)
 
-//Compressor.saveDataAsCompressedFile(imageData!, path: outputURL.path!)
+Compressor.saveDataAsCompressedFile(imageData!, path: outputURL.path!)
 
-let uncompressed = Compressor.loadCompressedFile(NSBundle.mainBundle().pathForResource("dark_road_small", ofType: "compressed")!)
-let image = UIImage(data: uncompressed!)
+//let uncompressed = Compressor.loadCompressedFile(NSBundle.mainBundle().pathForResource("dark_road_small", ofType: "compressed")!)
+//let image = UIImage(data: uncompressed!)
