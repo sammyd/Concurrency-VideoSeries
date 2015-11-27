267ea942b3eada80c2eda6e57ae19da0879df138
Sam Davies <sam@razeware.com>
2015-11-27 09:54:16 +0700

    002_Demo: Remove code to make the starter project

--- projects/002_NSOperationQueue/002_DemoStarter/TiltShift.playground/Contents.swift
+++ projects/002_NSOperationQueue/002_DemoStarter/TiltShift.playground/Contents.swift
@@ -6,24 +6,16 @@ XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
 //: NSOperationQueue is responsible for scheduling and running a set of operations, somewhere in the background. 
 
 //: To prevent the playground from killing background tasks when the main thread has completed, need to specify indefinite execution
-XCPlaygroundPage.currentPage.needsIndefiniteExecution = true
+
 
 
 //: ## Creating a queue
 //: Operations can be added to queues directly as closures
-let printerQueue = NSOperationQueue()
-printerQueue.maxConcurrentOperationCount = 2
-startClock()
-printerQueue.addOperationWithBlock { sleep(5); print("hello") }
-printerQueue.addOperationWithBlock { sleep(5); print("my") }
-printerQueue.addOperationWithBlock { sleep(5); print("name") }
-printerQueue.addOperationWithBlock { sleep(5); print("is") }
-printerQueue.addOperationWithBlock { sleep(5); print("sam") }
-stopClock()
 
-startClock()
-printerQueue.waitUntilAllOperationsAreFinished()
-stopClock()
+
+
+
+
 
 
 //: ## Adding NSOperations to queues
@@ -31,21 +23,15 @@ let images = ["city", "dark_road", "train_day", "train_dusk", "train_night"].map
 var filteredImages = [UIImage]()
 
 //: Create the queue with the default constructor
-let filterQueue = NSOperationQueue()
+
 
 //: Create a filter operations for each of the iamges, adding a completionBlock
 for image in images {
-  let filterOp = TiltShiftOperation()
-  filterOp.inputImage = image
-  filterOp.completionBlock = {
-    guard let output = filterOp.outputImage else { return }
-    filteredImages.append(output)
-  }
-  filterQueue.addOperation(filterOp)
+
 }
 
 //: Need to wait for the queue to finish before checking the results
-filterQueue.waitUntilAllOperationsAreFinished()
+
 
 //: Inspect the filtered images
 filteredImages
